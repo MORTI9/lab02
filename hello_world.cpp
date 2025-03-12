94b38fd7b3014d2cdb417a4899e5a35a11d090c5
MORTI9 <zaharov12345678902341@gmail.com>
2025-03-12 21:24:07 +0300

    style=Mozilla

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
-int main() {
-//Matvey Zakharov
-std::string name;
-std::cout << "Your name";
-std::cin >> name;
-std::cout << "Hello world from " << name;
+int
+main()
+{
+  // Matvey Zakharov
+  std::string name;
+  std::cout << "Your name";
+  std::cin >> name;
+  std::cout << "Hello world from " << name;
 }
