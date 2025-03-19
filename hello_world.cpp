a231107dd7d5f9cd3afe6b0ea5ac159574582184
MORTI9 <159233913+MORTI9@users.noreply.github.com>
2025-03-19 08:27:40 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,11 +2,7 @@
 #include <string>
 int main()
 {
-<<<<<<< HEAD
   // Матвей LALAL
-=======
-  // Lalallalalalala--=-=-
->>>>>>> b444d46 (new  codestyle)
   std::string name;
   std::cout << "Your name";
   std::cin >> name;
